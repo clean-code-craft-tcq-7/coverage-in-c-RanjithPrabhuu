9bae011dc0adcb4d04898350b0a06fd52ceeeae3
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 10:42:28 +0530

    Create breachCheck.c
    added breach check function

---
+++ breachCheck.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "breachCheck.c"
+
+//function definition to check the breach
+BreachType inferBreach(double value, double lowerLimit, double upperLimit) 
+{
+  if(value < lowerLimit) 
+  {
+    return TOO_LOW;
+  }
+  if(value > upperLimit) 
+  {
+    return TOO_HIGH;
+  }
+  return NORMAL;
+}
+
+//function definition to classify the limits
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
+{
+  int lowerLimit = limitValuesList[coolingType].lowerLimit;
+  int upperLimit = limitValuesList[coolingType].upperLimit;
+  return inferBreach(temperatureInC, lowerLimit, upperLimit);
+}
