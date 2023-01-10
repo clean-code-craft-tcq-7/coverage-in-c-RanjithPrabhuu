9db959c136c409eb3b9e9a8db67d328f29e8ab6f
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 11:59:53 +0530

    Update alertSend.h

--- alertSend.h
+++ alertSend.h
@@ -1,6 +1,10 @@
+//function pointer declaration
 typedef void (*alertFuncPtr)(BreachType, void (*alertMessage)(char*));
 
+//function declaration to send the messgage to controller
 void sendToController(BreachType breachType, void (*alertMessage)(char*));
+//function declaration to send the message via email
 void sendToEmail(BreachType breachType, void (*alertMessage)(char*));
 
+//extern for function pointer list
 extern alertFuncPtr alertTypeList[MAX_ALERT_TYPE];
