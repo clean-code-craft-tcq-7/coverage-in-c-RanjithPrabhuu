607bb484e688a8aa2b7912a970b5377aa2cbdf6e
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 14:41:57 +0530

    Update alertInfo.h

--- alertInfo.h
+++ alertInfo.h
@@ -1,2 +1,20 @@
-//function declaration for emailMessagealert
-extern const char* emailAlertMessageInEng[MAX_BREACH_TYPE];
+#include "config.h"
+
+#define MAXALERTTYPE 2
+
+typedef struct
+{
+	AlertTarget Target ;
+	void (*AlertCallback)(BreachType);
+}AlertTable;
+
+typedef struct
+{
+   BreachType breachtype;
+   const char*FirstMessage;
+   const char*SecondMessage;
+}sendToEmailMessage;
+
+void sendToController(BreachType breachType);
+void sendToEmail(BreachType breachType);
+void SendAlertMessage(AlertTarget alertTarget,BreachType breachType);
