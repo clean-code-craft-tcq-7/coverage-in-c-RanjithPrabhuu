b7a993f00f1627c221d2d3a27a4f53c9f217ec2a
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 15:03:48 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "config.h"
 
 //function declaration for check and alert function
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
