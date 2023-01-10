82e449610c613d51af87f22866dd4dae4f579ef4
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 11:36:08 +0530

    Create alertInfo.c
    created alertInfo.c file

---
+++ alertInfo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "config.h"
+
+const char* emailAlertMessageInEng[MAX_BREACH_TYPE] =
+{
+        "temperature is Normal\n",
+        "temperature is too low\n",
+        "temperature is too high\n"
+};
