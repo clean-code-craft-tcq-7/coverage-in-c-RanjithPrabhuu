231f5f3184cdd5dba6f3264db47e332e4c5fbf25
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 14:35:42 +0530

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,13 @@
 #pragma once
 
+#define NORMAL_COOLING_LOWERLIMIT 0
+#define NORMAL_COOLING_UPPERLIMIT 35
+#define HIGH_ACTIVE_COOLING_LOWERLIMIT 0
+#define HIGH_ACTIVE_COOLING_UPPERLIMIT 45
+#define MEDIUM_ACTIVE_COOLING_LOWERLIMIT 0
+#define MEDIUM_ACTIVE_COOLING_UPPERLIMIT 40
+#define MAXIMUM_COOLING_TYPE 3
+
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -28,3 +36,4 @@ typedef struct
 	int UpperLimit;
 }CoolingInfo;
 
+extern const CoolingInfo CoolingInfoTable[];
