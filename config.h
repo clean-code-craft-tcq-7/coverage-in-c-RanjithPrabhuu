adb05e7216e599542930feda334936608976697b
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 13:19:23 +0530

    Update config.h

--- config.h
+++ config.h
@@ -1,6 +1,5 @@
 #pragma once
 
-//enum for cooling type
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -8,7 +7,6 @@ typedef enum {
   MAX_COOLING_TYPE
 } CoolingType;
 
-//enum for breach type
 typedef enum {
   NORMAL,
   TOO_LOW,
@@ -16,27 +14,21 @@ typedef enum {
   MAX_BREACH_TYPE
 } BreachType;
 
-//structure to hold lower and upper limit
 typedef struct{
     int lowerLimit;
     int upperLimit;
 } LimitValue;
 
-//enum for alert mode 
 typedef enum {
   TO_CONTROLLER,
   TO_EMAIL,
   MAX_ALERT_TYPE,
 } AlertTarget;
 
-//structure to hold coolingType and brand
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
 
-//extern array to hold the limit
 extern LimitValue limitValuesList[MAX_COOLING_TYPE];
-//extern variable to hold the receipent
 extern const char* recepient;
-
