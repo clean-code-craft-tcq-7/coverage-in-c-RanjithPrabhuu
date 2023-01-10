352c0377639774a1d67d7337ee14cabd515e74d2
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 11:58:06 +0530

    Create alertSend.h
    created alertSend.h file

---
+++ alertSend.h
@@ -0,0 +1,6 @@
+typedef void (*alertFuncPtr)(BreachType, void (*alertMessage)(char*));
+
+void sendToController(BreachType breachType, void (*alertMessage)(char*));
+void sendToEmail(BreachType breachType, void (*alertMessage)(char*));
+
+extern alertFuncPtr alertTypeList[MAX_ALERT_TYPE];
