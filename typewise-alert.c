8f66156cacf498de14fd99a4bc4826fc4742972a
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-10 12:02:00 +0530

    Update typewise-alert.c

--- typewise-alert.c
+++ typewise-alert.c
@@ -1,32 +1,13 @@
 #include <stdio.h>
 #include "config.h"
 #include "breachCheck.h"
+#include "alertSend.h"
 #include "typewise-alert.h"
 
-
+//function definition for check and alert function
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC, void (*alertMessage)(char*))
 {
     BreachType breachType = classifyTemperatureBreach(batteryChar.coolingType, temperatureInC);
+    alertTypeList[alertTarget](breachType, alertMessage);
 }
  
-
-void sendToController(BreachType breachType) {
-  const unsigned short header = 0xfeed;
-  printf("%x : %x\n", header, breachType);
-}
-
-void sendToEmail(BreachType breachType) {
-  const char* recepient = "a.b@c.com";
-  switch(breachType) {
-    case TOO_LOW:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too low\n");
-      break;
-    case TOO_HIGH:
-      printf("To: %s\n", recepient);
-      printf("Hi, the temperature is too high\n");
-      break;
-    case NORMAL:
-      break;
-  }
-}
