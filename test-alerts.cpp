a1843efaa21014cb33ea7f3fd1b15deb3e24d950
RanjithPrabhuu <32065640+RanjithPrabhuu@users.noreply.github.com>
2023-01-17 11:14:22 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,14 +6,14 @@
 #include "breachCheck.h"
 
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+TEST_CASE("breach inference for low limit") {
+  REQUIRE(inferBreach(13, 20, 30) == TOO_LOW);
 }
-TEST_CASE("infers the breach according to High") {
-  REQUIRE(inferBreach(30, 10, 20) == TOO_HIGH);
+TEST_CASE("breach inference for high limit") {
+  REQUIRE(inferBreach(31, 10, 20) == TOO_HIGH);
 }
-TEST_CASE("infers the breach according to Normal") {
-  REQUIRE(inferBreach(20, 10, 40) == NORMAL);
+TEST_CASE("breach inference for normal limit") {
+  REQUIRE(inferBreach(21, 10, 40) == NORMAL);
 }
 TEST_CASE("classifyTemperatureBreach passive cooling") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 30) == NORMAL);
@@ -33,21 +33,33 @@ TEST_CASE("classifyTemperatureBreach acive cooling Too high") {
 TEST_CASE("classifyTemperatureBreach medactive cooling Too high") {
   REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 60) == TOO_HIGH);
 }  
+TEST_CASE("classifyTemperatureBreach passive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+}
+TEST_CASE("classifyTemperatureBreach acive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -2) == TOO_LOW);
+}
+TEST_CASE("classifyTemperatureBreach medactive cooling Too high") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -3) == TOO_LOW);
+}  
 
-char buf[50];
-TEST_CASE("send email alert high") {
-  const char*src = "Hi, the temperature is too high\n";
-  BatteryCharacter batteryCharTest;
-batteryCharTest.coolingType = PASSIVE_COOLING;
-checkAndAlert(TO_EMAIL,batteryCharTest,50,&buf[0]);
- REQUIRE(strcmp( buf,src)!=0);
-  }
+char buf[100];
 
-const char*src = "0xfeed : TOO_HIGH";
-TEST_CASE("send controller alert high") {
-  BatteryCharacter batteryCharTest;
-batteryCharTest.coolingType = HI_ACTIVE_COOLING;
-checkAndAlert(TO_CONTROLLER,batteryCharTest,60,&buf[0]);
- REQUIRE(strcmp( buf,src)!=0);
-  }
+TEST_CASE("send email alert high") 
+{
+  const char* src ="Hi, the temperature is too high\n";
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = PASSIVE_COOLING;
+  checkAndAlert(TO_EMAIL,batteryChar,50,&buf[0]);
+  REQUIRE(strcmp(buf,src)!=0);
+}
+
+const char* src = "0xfeed : TOO_HIGH";
+TEST_CASE("send controller alert high") 
+{
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = HI_ACTIVE_COOLING;
+  checkAndAlert(TO_CONTROLLER,batteryChar,60,&buf[0]);
+  REQUIRE(strcmp(buf,src)!=0);
+}
 
